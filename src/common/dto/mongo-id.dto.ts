550088865c8127c3de66c52e9ebebb0bcacd8664
nik-kita <nik.moiseienko@gmail.com>
2023-01-20 08:34:34 +0200

    fix: MongoIdDto -> only check, type still simple string

--- src/common/dto/mongo-id.dto.ts
+++ src/common/dto/mongo-id.dto.ts
@@ -1,13 +1,6 @@
-import { isValidObjectId, ObjectId, mongo } from 'mongoose';
-import { Transform } from 'class-transformer';
-import { BadRequestException } from '@nestjs/common';
+import { IsMongoId } from 'class-validator';
 
 export class MongoIdDto {
-  @Transform(({ value }) => {
-    if (isValidObjectId(value))
-      return mongo.ObjectId.createFromHexString(value);
-
-    throw new BadRequestException('_id should be a valid mongo-id string');
-  })
-  _id: ObjectId;
+  @IsMongoId()
+  _id: string;
 }
